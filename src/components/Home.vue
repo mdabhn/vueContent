bf2c72be093baab6b015d1d9c429e7c395588c24
md.abhn <md.abhn@gmail.com>
2020-06-08 20:14:05 +0600

    ...

---
+++ src/components/Home.vue
@@ -0,0 +1,14 @@
+<template>
+  <div class="container mt-4">
+    <h3 class="text-center">Welcome</h3>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "Home"
+};
+</script>
+
+<style>
+</style>
---
+++ src/components/Navbar.vue
@@ -0,0 +1,21 @@
+<template>
+  <nav class="navbar navbar-expand-lg navbar-light bg-light">
+    <a class="navbar-brand" href="#">Home</a>
+    <div class="collapse navbar-collapse" id="navbarSupportedContent">
+      <ul class="navbar-nav mr-auto">
+        <li class="nav-item">
+          <router-link class="nav-link">Create</router-link>
+        </li>
+      </ul>
+    </div>
+  </nav>
+</template>
+
+<script>
+export default {
+  name: "Navbar"
+};
+</script>
+
+<style>
+</style>
