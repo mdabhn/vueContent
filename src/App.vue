bb94a315599a808b46ba4ba5cd28d045a61c32f1
mdabhnx <iamhossainmdabhn@gmail.com>
2020-06-19 23:40:59 +0600

    init app

--- src/App.vue
+++ src/App.vue
@@ -1,37 +1,7 @@
 <template>
-  <div class="container mt-2">
-    <div class="input-group input-group-sm mb-3">
-      <input class="input-group" v-model="searchValue" type="text" />
-    </div>
-    <div v-for="(document, index) in search" :key="document.id">{{index + 1}}. {{ document.body }}</div>
+  <div id="app">
+    <router-view />
   </div>
 </template>
 
-<script>
-import axios from "axios";
-export default {
-  name: "app",
-  components: {},
-  data() {
-    return {
-      documents: [],
-      searchValue: ""
-    };
-  },
-  created() {
-    axios.get("https://jsonplaceholder.typicode.com/posts").then(res => {
-      this.documents = res.data;
-    });
-  },
-  computed: {
-    search() {
-      return this.documents.filter(data => {
-        return data.body.match(this.searchValue);
-      });
-    }
-  }
-};
-</script>
-
-<style>
-</style>
+<style></style>
---
+++ src/components/Home.vue
@@ -0,0 +1,13 @@
+<template>
+  <div class="container">
+    <p>Hello</p>
+  </div>
+</template>
+
+<script>
+  export default {
+    name: 'Home',
+  }
+</script>
+
+<style></style>
